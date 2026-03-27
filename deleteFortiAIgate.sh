1f0fc5a0bd90046361acb3b7b9c7f43a567a25a1
Sacha Dubois <sacha.dubois@icloud.com>
2026-03-27 11:15:07 +0100

    fixed installation problems caused by reinstalling the helm charts

--- deleteFortiAIgate.sh
+++ deleteFortiAIgate.sh
@@ -33,10 +33,12 @@ verifyAWSRoute53credentials
 messageTitle "Uninstall Open WebUI Packages"
 helm delete open-webui -n open-webui  > /dev/null 2>&1
 kubectl delete ns open-webui > /dev/null 2>&1
+waitForNamespaceDeletion open-webui
 
 messageTitle "Uninstall FortiAIgate Packages"
 helm delete fortiaigate -n fortiaigate > /dev/null 2>&1
 kubectl delete ns fortiaigate > /dev/null 2>&1
+waitForNamespaceDeletion fortiaigate
 
 messageTitle "Cleaning-up AWS EKS Kubernetes Cluster Deployment ($EKS_CLUSTER_NAME)"
 
@@ -44,21 +46,28 @@ messageTitle "Cleaning-up AWS EKS Kubernetes Cluster Deployment ($EKS_CLUSTER_NA
 # Step 1 - Delete application-facing resources first
 # ------------------------------------------------------------------------------------------
 deleteELBv2LoadBalancer
-deleteEKSnodeGroup
+#deleteEKSnodeGroup
 deleteEFSstorage
 
 # ------------------------------------------------------------------------------------------
-# Step 2 - Wait for AWS VPC resources to settle
+# Step 2 - Delete EKS cluster
+# ------------------------------------------------------------------------------------------
+deleteEKScluster
+waitForEKSclusterDeletion
+
+# ------------------------------------------------------------------------------------------
+# Step 3 - Wait for cluster CloudFormation stack and repair if needed
+# ------------------------------------------------------------------------------------------
+waitForClusterStackDeletion
+repairClusterStackIfNeeded
+
+# ------------------------------------------------------------------------------------------
+# Step 4 - Wait for AWS VPC resources to settle
 # ------------------------------------------------------------------------------------------
 waitForVpcResourcesToSettle
 
 # ------------------------------------------------------------------------------------------
-# Step 3 - Delete EKS cluster
-# ------------------------------------------------------------------------------------------
-deleteEKScluster
-
-# ------------------------------------------------------------------------------------------
-# Step 4 - Wait for cluster CloudFormation stack and repair if needed
+# Step 5 - Wait for cluster CloudFormation stack and repair if needed
 # ------------------------------------------------------------------------------------------
 waitForClusterStackDeletion
 repairClusterStackIfNeeded
